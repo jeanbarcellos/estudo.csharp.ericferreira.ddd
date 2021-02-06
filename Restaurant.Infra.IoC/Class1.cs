283dd0a61e299cfa0217f947db03c95572781bc5
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-06 20:59:44 -0300

    Codificação de Infra.IoC

--- Restaurant.Infra.IoC/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Restaurant.Infra.IoC
-{
-    public class Class1
-    {
-    }
-}
---
+++ Restaurant.Infra.IoC/DependencyInjector.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Restaurant.Application.Interfaces;
+using Restaurant.Application.Services;
+using Restaurant.Domain.Interfaces.Repositories;
+using Restaurant.Domain.Interfaces.Services;
+using Restaurant.Domain.Services;
+using Restaurant.Infra.Data.Repositories;
+using System;
+
+namespace Restaurant.Infra.IoC
+{
+    public class DependencyInjector
+    {
+        public static void Register(IServiceCollection svcCollection)
+        {
+            // Application
+            svcCollection.AddScoped(typeof(IAppBase<,>), typeof(ServiceAppBase<,>);
+            svcCollection.AddScoped<IDishApp, DishApp>();
+
+            // Domain           
+            svcCollection.AddScoped(typeof(IServiceBase<>), typeof(ServiceBase<>));
+            svcCollection.AddScoped<IDishService, DishService>();
+
+            // Repository
+            svcCollection.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+            svcCollection.AddScoped<IDishRepository, DishRepository>();
+        }
+    }
+}
